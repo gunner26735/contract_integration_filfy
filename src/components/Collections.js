3665e06ed84c3f7aa1fa1e90dc6490abb995af9b
gunner26735 <sidshukla2016@gmail.com>
2023-01-29 12:19:19 +0530

    adding files

---
+++ src/components/Collections.js
@@ -0,0 +1,22 @@
+import './collection_css.css'
+
+function Collections(){
+
+    
+
+
+    return(
+        <div className="Collections">
+            <h1>Collections are</h1>
+            <div className="tile">
+                <img class='tile-img' src="https://static.pexels.com/photos/9050/pexels-photo.jpg" />
+                <div class='tile-info'>
+                    <h1>Title</h1>
+                    <p>You can write anything you want here.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Collections;
